517359db3dd45f4eff0dbc3059db4a04531b1960
nolan <nolanhilbersgarcia@gmail.com>
2025-11-19 17:07:08 +0100

    vette shit

--- controller.py
+++ controller.py
@@ -1,15 +1,17 @@
 from pyPS4Controller.controller import Controller
-# from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
 from threading import Thread
 import numpy as np
 from time import sleep
-# kit = ServoKit(channels=16)
+kit = ServoKit(channels=16)
 
 
 X = 0
 Y = 0
 Z = 0
 MAX = 32766
+controlling = 0
+correcting = False
 
 
 class MyController(Controller):
@@ -46,9 +48,48 @@ class MyController(Controller):
         else:
             Y = 0
 
+    def on_R2_press(self, value):
+        global Z
+        if abs(value) > self.min_value:
+            Z = abs(value)/MAX
+
+    def on_R2_release(self):
+        global Z
+        Z = 0
+
+    def on_L2_press(self, value):
+        global Z
+        if abs(value) > self.min_value:
+            Z = -abs(value)/MAX
+
+    def on_L2_release(self):
+        global Z
+        Z = 0
+
+    def on_circle_press(self):
+        global correcting
+        global controlling
+        correcting = not correcting
+        controlling = 2
+
+    def on_square_press(self):
+        global correcting
+        global controlling
+        correcting = not correcting
+        controlling = 3
+
+    def on_triangle_press(self):
+        global correcting
+        global controlling
+        correcting = not correcting
+        controlling = 1
+
 
 def calculate_servo():
     while True:
+        if correcting:
+            print(f"Corecting {controlling}: {Y}")
+            kit.continuous_servo[controlling].throttle = Y
         serv1 = 0
         serv2 = 0
         serv3 = 0
@@ -74,8 +115,13 @@ def calculate_servo():
             serv3 = (1-ratio)*power
             serv2 = 0
         sleep(0.3)
+        serv1 += Z
+        serv3 += Z
+        serv2 += Z
         print(serv1,serv2,serv3)
-        # kit.continuous_servo[servo].throttle = value
+        kit.continuous_servo[0].throttle = serv1
+        kit.continuous_servo[1].throttle = serv2
+        kit.continuous_servo[2].throttle = serv3
 
 servoding = Thread(target= calculate_servo)
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
