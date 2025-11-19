2101f5a3c41a11ff6dc9144c6ad2c0b5ad60a25b
nolan <nolanhilbersgarcia@gmail.com>
2025-11-19 16:51:01 +0100

    los laten weggelaten

--- controller.py
+++ controller.py
@@ -19,24 +19,32 @@ class MyController(Controller):
         self.min_value = 3276
 
     def on_R3_left(self, value):
+        global X
         if abs(value) > self.min_value:
-            global X
             X = value/MAX
+        else:
+            X = 0
 
     def on_R3_right(self, value):
+        global X
         if abs(value) > self.min_value:
-            global X
             X = value/MAX
+        else:
+            X = 0
 
     def on_R3_up(self, value):
+        global Y
         if abs(value) > self.min_value:
-            global Y
             Y = value/MAX
+        else:
+            Y = 0
 
     def on_R3_down(self, value):
+        global Y
         if abs(value) > self.min_value:
-            global Y
             Y = value/MAX
+        else:
+            Y = 0
 
 
 def calculate_servo():
@@ -52,20 +60,22 @@ def calculate_servo():
             ratio = angle/120
             serv2 = ratio*power
             serv1 = (1-ratio)*power
-            serv3 = -power
+            serv3 = 0
         elif angle < 240:
             angle -= 120
             ratio = angle/120
             serv3 = ratio*power
             serv2 = (1-ratio)*power
-            serv1 = -power
+            serv1 = 0
         else:
             angle -= 240
             ratio = angle/120
             serv1 = ratio*power
             serv3 = (1-ratio)*power
-            serv2 = -power
-        sleep(0.1)
+            serv2 = 0
+        sleep(0.3)
+        print(serv1,serv2,serv3)
+        # kit.continuous_servo[servo].throttle = value
 
 servoding = Thread(target= calculate_servo)
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
