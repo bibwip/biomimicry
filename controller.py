0aadca55852dfa958566e3c07199f149d8cf5d78
nolan <nolanhilbersgarcia@gmail.com>
2025-11-27 11:21:28 +0100

    rewrote to POOP

--- controller.py
+++ controller.py
@@ -6,125 +6,113 @@ from time import sleep
 kit = ServoKit(channels=16)
 
 
-X = 0
-Y = 0
-Z = 0
-MAX = 32766
-controlling = 0
-correcting = False
-
-
+# Controller class that listens and handles the presses of the playstation
+# controller.
 class MyController(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
         self.min_value = 3276
+        self.self.max_value_value = 32766
+        self.X = 0
+        self.Y = 0
+        self.Z = 0
+        self.controlling = 0
+        self.correcting = False
+
 
     def on_R3_left(self, value):
-        global X
         if abs(value) > self.min_value:
-            X = value/MAX
+            self.X = value/self.max_value
         else:
-            X = 0
+            self.X = 0
 
     def on_R3_right(self, value):
-        global X
         if abs(value) > self.min_value:
-            X = value/MAX
+            self.X = value/self.max_value
         else:
-            X = 0
+            self.X = 0
 
     def on_R3_up(self, value):
-        global Y
         if abs(value) > self.min_value:
-            Y = value/MAX
+            self.Y = value/self.max_value
         else:
-            Y = 0
+            self.Y = 0
 
     def on_R3_down(self, value):
-        global Y
         if abs(value) > self.min_value:
-            Y = value/MAX
+            self.Y = value/self.self.max_value_value
         else:
-            Y = 0
+            self.Y = 0
 
     def on_R2_press(self, value):
-        global Z
         if abs(value) > self.min_value:
-            Z = abs(value)/MAX
+            self.Z = abs(value)/self.max_value
 
     def on_R2_release(self):
-        global Z
-        Z = 0
+        self.Z = 0
 
     def on_L2_press(self, value):
-        global Z
         if abs(value) > self.min_value:
-            Z = -abs(value)/MAX
+            self.Z = -abs(value)/self.max_value
 
     def on_L2_release(self):
-        global Z
-        Z = 0
+        self.Z = 0
 
     def on_circle_press(self):
-        global correcting
-        global controlling
-        correcting = not correcting
-        controlling = 2
+        self.correcting = not self.correcting
+        self.controlling = 2
 
     def on_square_press(self):
-        global correcting
-        global controlling
-        correcting = not correcting
-        controlling = 3
+        self.correcting = not self.correcting
+        self.controlling = 3
 
     def on_triangle_press(self):
-        global correcting
-        global controlling
-        correcting = not correcting
-        controlling = 1
+        self.correcting = not self.correcting
+        self.controlling = 1
 
-
-def calculate_servo():
-    while True:
-        if correcting:
-            print(f"Corecting {controlling}: {Y}")
-            kit.continuous_servo[controlling].throttle = Y/2 +0.1
-        serv1 = 0
-        serv2 = 0
-        serv3 = 0
-
-        power = np.sqrt(Y**2 + X**2)
-        angle = (np.pi + np.atan2(-X, Y)) * 180 / np.pi;
-
-        if angle < 120:
-            ratio = angle/120
-            serv2 = ratio*power
-            serv1 = (1-ratio)*power
-            serv3 = 0
-        elif angle < 240:
-            angle -= 120
-            ratio = angle/120
-            serv3 = ratio*power
-            serv2 = (1-ratio)*power
+    # Calculates the throttle speed of each servo to move the arm a certain
+    # direction.
+    def calculate_servo(self):
+        while True:
+            if self.correcting:
+                print(f"Corecting {self.controlling}: {self.Y}")
+                kit.continuous_servo[self.controlling].throttle = self.Y/2 +0.1
             serv1 = 0
-        else:
-            angle -= 240
-            ratio = angle/120
-            serv1 = ratio*power
-            serv3 = (1-ratio)*power
             serv2 = 0
-        sleep(0.3)
-        serv1 += Z
-        serv3 += Z
-        serv2 += Z
-        print(serv1,serv2,serv3)
-        kit.continuous_servo[0].throttle = (serv1/2) + 0.1
-        kit.continuous_servo[1].throttle = serv2/2 + 0.1
-        kit.continuous_servo[2].throttle = serv3/2 + 0.1
+            serv3 = 0
 
-servoding = Thread(target= calculate_servo)
+            power = np.sqrt(self.Y**2 + self.X**2)
+            angle = (np.pi + np.atan2(-self.X, self.Y)) * 180 / np.pi;
+
+            if angle < 120:
+                ratio = angle/120
+                serv2 = ratio*power
+                serv1 = (1-ratio)*power
+                serv3 = 0
+            elif angle < 240:
+                angle -= 120
+                ratio = angle/120
+                serv3 = ratio*power
+                serv2 = (1-ratio)*power
+                serv1 = 0
+            else:
+                angle -= 240
+                ratio = angle/120
+                serv1 = ratio*power
+                serv3 = (1-ratio)*power
+                serv2 = 0
+            sleep(0.3)
+            serv1 += self.Z
+            serv3 += self.Z
+            serv2 += self.Z
+            print(serv1,serv2,serv3)
+            kit.continuous_servo[0].throttle = (serv1/2) + 0.1
+            kit.continuous_servo[1].throttle = serv2/2 + 0.1
+            kit.continuous_servo[2].throttle = serv3/2 + 0.1
+
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+servoding = Thread(target= controller.calculate_servo)
 
 servoding.start()
 controller.listen()
---
+++ old.py
@@ -0,0 +1,130 @@
+from pyPS4Controller.controller import Controller
+from adafruit_servokit import ServoKit
+from threading import Thread
+import numpy as np
+from time import sleep
+kit = ServoKit(channels=16)
+
+
+X = 0
+Y = 0
+Z = 0
+MAX = 32766
+controlling = 0
+correcting = False
+
+
+class MyController(Controller):
+
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+        self.min_value = 3276
+
+    def on_R3_left(self, value):
+        global X
+        if abs(value) > self.min_value:
+            X = value/MAX
+        else:
+            X = 0
+
+    def on_R3_right(self, value):
+        global X
+        if abs(value) > self.min_value:
+            X = value/MAX
+        else:
+            X = 0
+
+    def on_R3_up(self, value):
+        global Y
+        if abs(value) > self.min_value:
+            Y = value/MAX
+        else:
+            Y = 0
+
+    def on_R3_down(self, value):
+        global Y
+        if abs(value) > self.min_value:
+            Y = value/MAX
+        else:
+            Y = 0
+
+    def on_R2_press(self, value):
+        global Z
+        if abs(value) > self.min_value:
+            Z = abs(value)/MAX
+
+    def on_R2_release(self):
+        global Z
+        Z = 0
+
+    def on_L2_press(self, value):
+        global Z
+        if abs(value) > self.min_value:
+            Z = -abs(value)/MAX
+
+    def on_L2_release(self):
+        global Z
+        Z = 0
+
+    def on_circle_press(self):
+        global correcting
+        global controlling
+        correcting = not correcting
+        controlling = 2
+
+    def on_square_press(self):
+        global correcting
+        global controlling
+        correcting = not correcting
+        controlling = 3
+
+    def on_triangle_press(self):
+        global correcting
+        global controlling
+        correcting = not correcting
+        controlling = 1
+
+
+def calculate_servo():
+    while True:
+        if correcting:
+            print(f"Corecting {controlling}: {Y}")
+            kit.continuous_servo[controlling].throttle = Y/2 +0.1
+        serv1 = 0
+        serv2 = 0
+        serv3 = 0
+
+        power = np.sqrt(Y**2 + X**2)
+        angle = (np.pi + np.atan2(-X, Y)) * 180 / np.pi;
+
+        if angle < 120:
+            ratio = angle/120
+            serv2 = ratio*power
+            serv1 = (1-ratio)*power
+            serv3 = 0
+        elif angle < 240:
+            angle -= 120
+            ratio = angle/120
+            serv3 = ratio*power
+            serv2 = (1-ratio)*power
+            serv1 = 0
+        else:
+            angle -= 240
+            ratio = angle/120
+            serv1 = ratio*power
+            serv3 = (1-ratio)*power
+            serv2 = 0
+        sleep(0.3)
+        serv1 += Z
+        serv3 += Z
+        serv2 += Z
+        print(serv1,serv2,serv3)
+        kit.continuous_servo[0].throttle = (serv1/2) + 0.1
+        kit.continuous_servo[1].throttle = serv2/2 + 0.1
+        kit.continuous_servo[2].throttle = serv3/2 + 0.1
+
+servoding = Thread(target= calculate_servo)
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+
+servoding.start()
+controller.listen()
